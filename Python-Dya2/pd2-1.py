d5b88cbc1856bfb3cee3a5bc8aed38d9a4e5e07e
5ma111eaf <5ma111eaf404@gmail.com>
2024-06-15 12:23:15 +0800

    第二次提交

---
+++ Python-Dya2/pd2-1.py
@@ -0,0 +1,26 @@
+print("Day 2: 30Days of python programming")
+
+first_name = '5ma11'
+
+last_name = '1eaf'
+
+full_name = '5ma111eaf'
+
+country = 'chine'
+
+city = 'chine'
+
+age = 20
+
+year = 2024
+
+is_married = False
+
+is_true = 'yes'
+
+is_light_on = 'off'
+
+
+first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light_on = '5ma11', '1eaf', '5ma111eaf', 'chine', 'chine', 20, 2024, False, 'yes', 'off'
+
+print(first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light_on)
---
+++ Python-Dya2/pd2-2.py
@@ -0,0 +1,15 @@
+# 于大部分都是string类型所以我取部分来检测它们的数据类型
+
+city = 'chine'
+age = 20
+is_married = False
+is_true = 'yes'
+
+print(type(city), type(age), type(is_married), type(is_true))
+
+len('5ma11')
+len('1eaf')
+if len('5ma11') == len('1eaf')
+    {
+        print("长度相同")
+}
